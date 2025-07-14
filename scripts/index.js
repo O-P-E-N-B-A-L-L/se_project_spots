eb94f6f0ccc34304e59d0bc2d8779d6558637457
AndyS <andyscheffner@outlook.com>
2025-07-14 18:52:15 -0500

    add skeleton for dynamic card rendering

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,37 @@
        This will result in the logging of 'New post using image "" with caption "caption" was created.'
 
 */
+// --- --------------- --- //
+// --- Local Variables --- //
+// --- --------------- --- //
+
+const initialCards = [
+  {
+    name: "Val Thorens",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
+  {
+    name: "Restaurant terrace",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
+  },
+  {
+    name: "An outdoor cafe",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
+  },
+  {
+    name: "A very long bridge, over the forest and through the trees",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
+  },
+  {
+    name: "Tunnel with morning light",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
+  },
+  {
+    name: "Mountain house",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
+  },
+];
+
 // --- ---------------------- --- //
 // --- Element Identification --- //
 // --- ---------------------- --- //
@@ -76,31 +107,39 @@ function addNewPost() {
 // --- Event Listeners --- //
 // --- --------------- --- //
 
-editProfileBtn.addEventListener("click", function () {
+editProfileBtn.addEventListener("click", () => {
   toggleModalVisibility(editProfileModal);
   fillProfileInputFields();
 });
 
-editProfileCloseBtn.addEventListener("click", function () {
+editProfileCloseBtn.addEventListener("click", () => {
   toggleModalVisibility(editProfileModal);
 });
 
-editProfileForm.addEventListener("submit", function (e) {
+editProfileForm.addEventListener("submit", (e) => {
   e.preventDefault();
   setProfileContentFields();
   toggleModalVisibility(editProfileModal);
 });
 
-newPostBtn.addEventListener("click", function () {
+newPostBtn.addEventListener("click", () => {
   toggleModalVisibility(newPostModal);
 });
 
-newPostCloseBtn.addEventListener("click", function () {
+newPostCloseBtn.addEventListener("click", () => {
   toggleModalVisibility(newPostModal);
 });
 
-newPostForm.addEventListener("submit", function (e) {
+newPostForm.addEventListener("submit", (e) => {
   e.preventDefault();
   addNewPost();
   toggleModalVisibility(newPostModal);
+});
+
+// --- ---------- --- //
+// --- Load Posts --- //
+// --- ---------- --- //
+
+initialCards.forEach((card, index) => {
+  console.log(`Array item loaded: "${card.name}".`);
 });
